53d889ef35e17df6e386da4909c61b9c90814905
Арсений Конколович <333@onlinepbx.ru>
2023-01-25 16:19:35 +0500

    Fix some

--- cycles/lineIntersection.js
+++ cycles/lineIntersection.js
@@ -1,11 +1,6 @@
-const str1 = "asdfgh"
-const str2 = "fghjkl"
-
 const lineIntersection = (str1, str2) => {
   return str1
-    .split("")
+    .split('')
     .filter((word) => str2.includes(word))
-    .join("")
+    .join('')
 }
-
-console.log(lineIntersection(str1, str2))
--- cycles/traversingTreeWithoutRecursion.js
+++ cycles/traversingTreeWithoutRecursion.js
@@ -37,5 +37,3 @@ const traversal = (tree) => {
     }
   }
 }
-
-traversal(tree)
