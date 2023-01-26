2bf86adde78edfb208e6472cdd22f02f2fd9787f
Арсений Конколович <333@onlinepbx.ru>
2023-01-26 18:06:20 +0500

    Add common export

---
+++ src/conditions/index.ts
@@ -0,0 +1,8 @@
+import { isMoreThan10, isMoreThan10andEven } from './moreThan10AndEven'
+
+const condition = {
+  isMoreThan10,
+  isMoreThan10andEven,
+}
+
+export default condition
---
+++ src/cycles/index.ts
@@ -0,0 +1,9 @@
+// import isOrderByASC from './isOrderByASC'
+import lineIntersection from './lineIntersection'
+
+const cycles = {
+  // isOrderByASC,
+  lineIntersection,
+}
+
+export default cycles
