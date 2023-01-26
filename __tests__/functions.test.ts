7265a29afa7520f2c851624a4702acc8651ce32d
Арсений Конколович <333@onlinepbx.ru>
2023-01-26 18:06:46 +0500

    Add tests for condition and cycles

--- __tests__/functions.test.ts
+++ __tests__/functions.test.ts
@@ -1,8 +1,35 @@
-// import { isMoreThan10 } from '../src/conditions/moreThan10AndEven'
-const isMoreThan10 = require('../src/conditions/moreThan10AndEven')
+import conditions from '../src/conditions/index'
+import cycles from '../src/cycles/index'
 
-describe('sum module', () => {
-  test('adds 1 + 2 to equal 3', () => {
-    expect(isMoreThan10(7)).toBeFalsy()
+describe('Condition', () => {
+  test('Is more than 10', () => {
+    const expected = conditions.isMoreThan10(12)
+    expect(expected).toBeTruthy()
+  })
+
+  test('Is more than 10 and Even', () => {
+    const expected = conditions.isMoreThan10andEven(12)
+    expect(expected).toBeTruthy()
+  })
+})
+
+describe('Cycles', () => {
+  // test('Is order by ASC', () => {
+  //   const numASC = [1, 5, 9, 10]
+  //   const strASC = ['a', 'b', 'e', 'r']
+  //   const randomNum = [1, 5, 9, 38]
+  //   const randomStr = ['b', 'p', 'a', 'z']
+  //   expect(cycles.isOrderByASC(numASC)).toBeTruthy()
+  //   expect(cycles.isOrderByASC(strASC)).toBeTruthy()
+  //   expect(cycles.isOrderByASC(randomNum)).toBeFalsy()
+  //   expect(cycles.isOrderByASC(randomStr)).toBeFalsy()
+  // })
+
+  test('Line intersection', () => {
+    const str1 = 'asdfgh'
+    const str2 = 'fghjkl'
+
+    expect(cycles.lineIntersection(str1, str2)).toEqual('fgh')
+    expect(cycles.lineIntersection(str1, 'zzzzz')).toEqual('')
   })
 })
