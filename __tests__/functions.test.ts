e6c1097702acb1f551f2a8d140f79fea1bfd9d6a
Арсений Конколович <333@onlinepbx.ru>
2023-01-26 15:09:31 +0500

    Add jest

---
+++ __tests__/functions.test.ts
@@ -0,0 +1,7 @@
+import { isMoreThan10 } from '../src/conditions/moreThan10AndEven'
+
+describe('sum module', () => {
+  test('adds 1 + 2 to equal 3', () => {
+    expect(isMoreThan10(7)).toBeFalsy()
+  })
+})
